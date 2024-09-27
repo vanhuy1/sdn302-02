789671e1f3bf7daa2119c4604f66525c80b8f62d
Van Huy <thichanpho13@gmail.com>
2024-09-27 21:19:40 +0700

    func Login, Routes, useAuth hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/home';
-import {Route, Routes} from 'react-router-dom';
+import HomePage from './features/auth/home';
+import { Route, Routes } from 'react-router-dom';
+import Login from './features/auth/Login'
+import Public from './components/Public';
+import Layout from './components/Layout';
+import PersistLogin from './features/auth/PersistLogin'
+import RequireAuth from './features/auth/RequireAuth'
+import { ROLES } from './config/roles'
+import Prefetch from './features/auth/Prefetch'
+import DashLayout from './components/DashLayout'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path='/' element={<Layout />} >
+        {/* public routes */}
+        <Route index element={<Public />} />
+        <Route path='login' element={<Login />} />
+
+        {/* Protected Routes */}
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}>
+            <Route element={<Prefetch />}>
+              <Route path="dash" element={<DashLayout />}>
+
+                <Route index element={<HomePage />} />
+
+              </Route>
+
+
+            </Route>
+          </Route>
+        </Route>
+
+      </Route>
     </Routes>
   );
 }
---
+++ client/src/components/landing-page/header.js
@@ -0,0 +1,108 @@
+// NavbarComponent.js
+import React from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+import { useSendLogoutMutation } from '../../features/auth/authApiSlice'
+import { useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faFileCirclePlus,
+    faFilePen,
+    faUserGear,
+    faUserPlus,
+    faRightFromBracket
+} from "@fortawesome/free-solid-svg-icons"
+import useAuth from '../../hooks/useAuth'
+import PulseLoader from 'react-spinners/PulseLoader'
+
+const DASH_REGEX = /^\/dash(\/)?$/
+
+const Header = () => {
+
+    const { username, status } = useAuth();
+
+    const { isManager } = useAuth()
+
+    const navigate = useNavigate()
+    const pathname = useLocation()
+
+    const [sendLogout, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useSendLogoutMutation()
+
+    useEffect(() => {
+        if (isSuccess) navigate('/')
+    }, [isSuccess, navigate])
+
+    const logoutButton = (
+        <button
+            className="icon-button"
+            title="Logout"
+            onClick={sendLogout}
+        >
+            <FontAwesomeIcon icon={faRightFromBracket} />
+        </button>
+    )
+
+    let dashClass = null
+    if (!DASH_REGEX.test(pathname)) {
+        dashClass = "dash-header__container--small"
+    }
+
+    const errClass = isError ? "errmsg" : "offscreen"
+
+    let buttonContent
+    if (isLoading) {
+        buttonContent = <PulseLoader color={"#FFF"} />
+    } else {
+        buttonContent = (
+            <>
+
+                {logoutButton}
+            </>
+        )
+    }
+    return (
+        <>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#">
+                    <img
+                        src="https://img.freepik.com/premium-vector/hotel-icon-logo-vector-design-template_827767-3569.jpg"
+                        alt="Logo"
+                        width="50"
+                        height="50"
+                        className="d-inline-block align-top"
+                        style={{ marginLeft: '30px' }}
+                    />{' '}
+                </Navbar.Brand>
+
+                <h2 style={{ marginLeft: '100px' }}> CODER HOTEL </h2>
+
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" style={{ marginRight: '30px' }}>
+                    <Nav className="me-5">
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/services">Services</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+                    </Nav>
+
+                    {/* <Button variant="outline-primary" className="me-2">Register</Button>
+                    <Button variant="primary">
+                        Login
+                    </Button> */}
+                    <nav className="dash-header__nav">
+                        <p>Current User: {username}</p>
+                        <p>Status: {status}</p>
+                        {buttonContent}
+                    </nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+        </>
+    );
+};
+
+export default Header;
